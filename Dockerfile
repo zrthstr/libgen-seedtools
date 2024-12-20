fc29b58237620e1d691952d14802d65116d19305
zrthstr <zrth1k@gmail.com>
2024-12-20 23:51:14 +0100

    fix test

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,9 @@ WORKDIR /app
 
 RUN uv sync --frozen
 RUN uv build
+#RUN uv run pytest
 ENV PATH="/app/.venv/bin:$PATH"
 
-CMD ["pytest", "--maxfail=1", "--disable-warnings", "-v", "--tb=long", "-s"]
+CMD ["uv", "run", "pytest"]
+
+#CMD ["pytest", "--maxfail=1", "--disable-warnings", "-v", "--tb=long", "-s"]
--- tests/test_fullrun.py
+++ tests/test_fullrun.py
@@ -9,7 +9,6 @@ config_path = "/app/config.json"
 config_schema_version = 1.1
 transmission_rpc_min_version = 16
 
-
 def edit_config():
     with open(config_path, "r") as file:
         config = json.load(file)
@@ -20,23 +19,50 @@ def edit_config():
 
 
 def test_config():
+    result = subprocess.run(
+        [name, "generate-config", "-t", "http://172.17.0.1:9091"],
+        capture_output=True,
+        text=True,
+    )
+    assert result.returncode == 0, f"Got:\n{result.stderr}"
+
     with open(config_path, "r") as file:
+        #print(file.read())
         config = json.load(file)
     assert config["version"] == config_schema_version
 
 
+def test_version():
+    result = subprocess.run(
+        [name, "--version"],
+        capture_output=True,
+        text=True,
+    )
+    assert result.returncode == 0
+    assert f"{name}, version" in result.stdout.lower()
+
+
 def test_transmission_rpc():
-    client = transmissionrpc.Client(
-        "172.17.0.1", port=9091, user="username", password="password"
+    client = transmission_rpc.Client(
+        host="172.17.0.1", port=9091, username="username", password="password"
     )
     assert client.get_session() is not None
-    assert client.rpc_version > transmission_rpc_min_version
+    assert client.get_session().rpc_version > transmission_rpc_min_version
 
 
 def test_fetch():
     assert edit_config() == True
     result = subprocess.run(
         [name, "fetch", "--auto-verify"],
+        capture_output=True,
+        text=True,
+    )
+    assert result.returncode == 0, f"Got:\n{result.stderr}"
+
+
+def test_dryrun():
+    result = subprocess.run(
+        [name, "fetch", "--dry-run"],
         capture_output=True,
         text=True,
     )
